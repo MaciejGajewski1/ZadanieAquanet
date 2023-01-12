d2fb08a789d0344b833022912d7e06c44183e5cb
MaciejGajewski1 <mgajewski101@gmail.com>
2023-01-12 13:45:02 +0100

    add basic Spring configurated app

---
+++ src/main/java/pl/aquanet/ZadanieAquanet/ZadanieAquanetApplication.java
@@ -0,0 +1,13 @@
+package pl.aquanet.ZadanieAquanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieAquanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadanieAquanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/aquanet/ZadanieAquanet/ZadanieAquanetApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aquanet.ZadanieAquanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieAquanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
